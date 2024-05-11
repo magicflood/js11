1b1cb451675de07f87a650db6693137b24ad9bc2
SaidamirRahimtorayev <saidamirxonrahimtorayev@gmail.com>
2024-05-11 21:03:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// let c;
+
+// function matem(a, b) {
+//     c = a + b;
+//     console.log(c);
+// }
+// matem(a, b);
+
+
+
+// function name11() {
+//     let a = 12;
+//     alert(a);
+//     alert(typeof (a));
+// }
+// name11();
+
+
+
+// function name() {
+//     let a = Math.trunc(100 * Math.random());
+//     alert(a);
+//     if (a % 2 === 0) {
+//         alert(a + " Juft")
+//     } else {
+//         alert(a + " Toq")
+//     }
+// }
+// name();
+
+
+
+// function name() {
+//     let a = prompt("Birorta so'z yozing...")
+//     let b = "Mars It School";
+
+//     if (a === "Mars") {
+//         alert(a);
+//     } else {
+//         alert(b);
+//     }
+// }
+// name();
+
+
+
+// let a = ["html.index", "css", "java script", "java", "pythonn"];
+// let b = [];
+// function name2() {
+
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i].length > 5) {
+//             b.push(a[i]);
+//         }
+//     }
+//     console.log(b);
+// }
+// name2();
+
+
+
+// function sto() {
+//     for (let i = 0; i < 100; i++) {
+//         if (i % 2 === 0) {
+//             console.log(i + " juft");
+//         } else {
+//             console.log(i + " toq");
+//         }
+
+//     }
+// }
+// sto();
